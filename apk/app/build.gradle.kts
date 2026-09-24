eed3ba55f65883de9b9a0755b9c849ba43e3bea4
homsi777 <sezarooo700@gmail.com>
2026-09-25 00:07:10 +0300

    Bump Android version to 2.0 (build 6) for the home screen redesign release

--- apk/app/build.gradle.kts
+++ apk/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
     applicationId = "com.aistudio.agentapp.abooerp"
     minSdk = 24
     targetSdk = 35
-    versionCode = 5
-    versionName = "1.2"
+    versionCode = 6
+    versionName = "2.0"
     multiDexEnabled = true
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
